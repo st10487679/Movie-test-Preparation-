c00a5dc2d0a074cbf6246b3cc13b76a9a968276d
st10487679 <st10487679@myemeris.edu.za>
2026-01-27 23:48:31 +0200

    Arrays of the movie ticket sales

---
+++ MoviesPrep/src/main/java/com/mycompany/moviesprep/MoviesPrep.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.moviesprep;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class MoviesPrep {
+
+    public static void main(String[] args) {
+        // to print when given the values 
+        
+        int [][] ticketSales = {{2100,1800,2400},
+                              {3200,2600,2900}};
+      String [] months = {"JAN","FEB","MAR"};
+      String [] name = {"Avatar","Titanic"};
+      
+      //to input the values
+      int [][] movieInput = new int[2][3];
+      Scanner sc = new Scanner(System.in);
+      
+      
+      //To populate the array 
+      
+      
+        for (int i = 0; i < name.length; i++) {
+            
+            for (int j = 0; j < months.length; j++) {
+                System.out.println("Enter the Ticket sold for "+ name[i] + " for " +months[j]);
+            
+            int userInput = sc.nextInt();
+            
+            movieInput[i][j] = userInput;
+            
+            }
+        
+        }
+                 
+               
+      //printing tha Array
+        System.out.println(" MOVIE TICKET SALES REPORT ");
+        
+        System.out.println("\t" + months[0] +"\t" + months[1] +"\t" + months[2]);   // to print the 1st row ( months)
+      
+        for (int i = 0; i < name.length ; i++) {  // to print the 1st column (movie name)    
+            System.out.print(name[i]); 
+            
+            for (int j = 0; j < movieInput[i].length; j++) {    // to print the values in the 
+                System.out.print("\t" + movieInput[i][j]);
+                
+            }
+            System.out.println(""); //to print the second movie 
+        }
+        
+        
+        
+        
+                 
+                 
+                
+            }
+        }
+    
+
